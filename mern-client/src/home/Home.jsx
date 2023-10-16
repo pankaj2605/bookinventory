9d0360dc21afa8f9c741166967b12b9e70c6cd55
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-16 12:51:30 +0530

    completed homepage with footer and customer review

--- mern-client/src/home/Home.jsx
+++ mern-client/src/home/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import BestsellerBooks from './BestSellerBooks'
+import FavBook from './FavBook'
+import PromoBanner from './PromoBanner'
+import OtherBooks from './OtherBooks'
+import Review from './Review'
 
 export default function Home() {
   return (
@@ -8,6 +12,10 @@ export default function Home() {
         
         <Banner/>
         <BestsellerBooks/>
+        <FavBook/>
+        <PromoBanner/>
+        <OtherBooks/>
+        <Review/>
     </div>
   )
 }
